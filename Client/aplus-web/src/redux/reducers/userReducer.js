16a958b5872abb4a9f300b6e5fe817842100d000
Shalitha <shalithax@gmail.com>
2020-03-10 16:06:20 +0530

    implemented asiox call

--- Client/aplus-web/src/redux/reducers/userReducer.js
+++ Client/aplus-web/src/redux/reducers/userReducer.js
@@ -9,8 +9,6 @@ export default function(state = initialState, action)
 		switch (action.type)
 		{
 			case DO_LOGIN :
-				console.log('passed');
-				console.log(action.payload);
 				return {
 					...state, items : action.payload
 				}
--- Client/aplus-web/src/redux/userActions.js
+++ Client/aplus-web/src/redux/userActions.js
@@ -2,17 +2,35 @@ import { DO_LOGIN, GET_TOKEN } from './actionTypes';
 import axios from 'axios';
 import { LOGIN_ENDPOINT } from '../config';
 
-export const doLogin = (email, password) => (dispatch) =>
+export const doLogin = (email, password) => async (dispatch) =>
 {
-	axios({
+	let success = false;
+	let resData;
+	await axios({
 		method : 'post',
 		url    : LOGIN_ENDPOINT,
 		data   : {
 			email : email, password : password
 		}
-	}).then((res) => dispatch({
-			type    : DO_LOGIN,
-			payload : res.data
-		}));
+	})
+		.then(function(response)
+		{
+			resData = response.data;
+			success = true;
+		})
+		.catch(function(error)
+		{
+			resData = error;
+			console.log(error);
+		});
 
+	if(success){
+		dispatch({
+			type    : DO_LOGIN,
+			payload : resData
+		});
+	}else{
+		return { "success": success, "data": resData, "error": true };
+	}
+	return { "success": success, "data": null, "error": false };
 }
