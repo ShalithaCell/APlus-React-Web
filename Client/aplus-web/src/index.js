36f1c4a961cadc60b3296ed12f8e5a469cf48981
Shalitha <shalithax@gmail.com>
2020-03-10 09:32:17 +0530

    implemented basic redux

--- Client/aplus-web/src/index.js
+++ Client/aplus-web/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import reducers from './redux/reducers/index';
+
+// use applyMiddleware to add the thunk middleware to the store
+const store = createStore(reducers, applyMiddleware(thunk));
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>, 
+	document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ Client/aplus-web/src/redux/actionTypes.js
@@ -0,0 +1,2 @@
+export const DO_LOGIN = 'DO_LOGIN';
+export const GET_TOKEN = 'GET_TOKEN';
