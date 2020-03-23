d257731943349b0f8649db678154d9aacc29c894
Shalitha <shalithax@gmail.com>
2020-03-23 20:13:23 +0530

    implemented role api

--- API/Portal.API/Startup.cs
+++ API/Portal.API/Startup.cs
@@ -143,7 +143,6 @@ namespace Portal.API
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
-            app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseAuthentication();
