2c1891658e238f31b2c85e51af6d5d2d1555766b
Chehan-nethsara <chehan4@gmail.com>
2020-03-24 23:05:33 +0530

    Its include the main interface of POS System & AddSupplier and InformTo Supplier interfaces

--- Client/aplus-web/src/App.js
+++ Client/aplus-web/src/App.js
@@ -4,7 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 import home from './component/home'
 import login from './component/login'
-import sales from '../src/sales'
 import addSupplier from './component/addSupplier'
 import informSupplier from './component/informSupplier'
 import sales_homes from './component/sales_home'
@@ -17,7 +16,6 @@ class App extends Component {
             <Switch>
                 <Route path='/home' component={ home } />
                 <Route path='/login' component={ login } />
-                <Route path='/sales' component={ sales } />
                 <Route path='/addSupplier' component={ addSupplier } />
                 <Route path='/informSupplier' component={ informSupplier } />
                 <Route path='/sales_homes' component={ sales_homes } />
--- Client/aplus-web/src/component/sales_home.js
+++ Client/aplus-web/src/component/sales_home.js
@@ -4,15 +4,12 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const useStyles = makeStyles((theme) => ({
