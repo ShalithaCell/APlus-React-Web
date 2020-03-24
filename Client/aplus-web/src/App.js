8f0a69aa4fbb7c24e3f715ad3bfba6d2dc85d732
Chehan-nethsara <chehan4@gmail.com>
2020-03-24 22:55:02 +0530

    its include the main interface of pos system and addSuppliers  and to inform supplier message interfaces

--- Client/aplus-web/src/App.js
+++ Client/aplus-web/src/App.js
@@ -4,6 +4,10 @@ import logo from './logo.svg';
 import './App.css';
 import home from './component/home'
 import login from './component/login'
+import sales from '../src/sales'
+import addSupplier from './component/addSupplier'
+import informSupplier from './component/informSupplier'
+import sales_homes from './component/sales_home'
 
 class App extends Component {
 	render(){
@@ -13,6 +17,11 @@ class App extends Component {
             <Switch>
                 <Route path='/home' component={ home } />
                 <Route path='/login' component={ login } />
+                <Route path='/sales' component={ sales } />
+                <Route path='/addSupplier' component={ addSupplier } />
+                <Route path='/informSupplier' component={ informSupplier } />
+                <Route path='/sales_homes' component={ sales_homes } />
+                
             </Switch>
         </div>
     </BrowserRouter>
