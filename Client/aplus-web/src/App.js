8db9fc29f633016551d7c06a5a83f78cf83f59d5
VinuriHemalka <hemalkavinuri123@gmail.com>
2020-03-24 21:54:25 +0530

    Create AddRequest.js,UpdateRequest.js,Attendance.js and request.js files

--- Client/aplus-web/src/App.js
+++ Client/aplus-web/src/App.js
@@ -4,6 +4,11 @@ import logo from './logo.svg';
 import './App.css';
 import home from './component/home'
 import login from './component/login'
+import Attendance from './component/Attendance'
+import EmployeeRequest from './component/EmployeeRequest'
+import AddRequest from './component/AddRequest'
+import UpdateRequest from './component/UpdateRequest'
+import request from './component/request'
 
 class App extends Component {
 	render(){
@@ -13,6 +18,11 @@ class App extends Component {
             <Switch>
                 <Route path='/home' component={ home } />
                 <Route path='/login' component={ login } />
+                <Route path='/Attendance' component={ Attendance } />
+                <Route path='/EmployeeRequest' component={ EmployeeRequest } />
+                <Route path='/AddRequest' component={ AddRequest } />
+                <Route path='/UpdateRequest' component={ UpdateRequest } />
+                <Route path='/request' component={ request } />
             </Switch>
         </div>
     </BrowserRouter>
