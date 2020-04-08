db09067374227d854c8b3e3e1e21e1eac296e3da
NethminiSAT <thimanethmini@gmail.com>
2020-04-08 12:17:56 +0530

    add inventory vs

---
+++ API/Portal.API/Migrations/20200402062015_addInventoryTable.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Portal.API.Migrations
+{
+    public partial class addInventoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegistedDate = table.Column<DateTime>(nullable: false, defaultValueSql: "GETDATE()"),
+                    IsActive = table.Column<bool>(nullable: false, defaultValue: true),
+                    ProductName = table.Column<string>(nullable: false),
+                    Qty = table.Column<int>(nullable: false),
+                    SupplireName = table.Column<string>(nullable: false),
+                    SupplireEmail = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.ID);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 673, DateTimeKind.Local).AddTicks(1194) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 2,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 674, DateTimeKind.Local).AddTicks(2187) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 3,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 674, DateTimeKind.Local).AddTicks(2216) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 4,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 674, DateTimeKind.Local).AddTicks(2219) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 5,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 674, DateTimeKind.Local).AddTicks(2221) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 6,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 674, DateTimeKind.Local).AddTicks(2223) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 7,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 4, 2, 11, 50, 14, 674, DateTimeKind.Local).AddTicks(2225) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 927, DateTimeKind.Local).AddTicks(899) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 2,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 929, DateTimeKind.Local).AddTicks(5818) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 3,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 929, DateTimeKind.Local).AddTicks(5864) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 4,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 929, DateTimeKind.Local).AddTicks(5867) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 5,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 929, DateTimeKind.Local).AddTicks(5869) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 6,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 929, DateTimeKind.Local).AddTicks(5870) });
+
+            migrationBuilder.UpdateData(
+                table: "customPermissions",
+                keyColumn: "ID",
+                keyValue: 7,
+                columns: new[] { "IsActive", "RegistedDate" },
+                values: new object[] { true, new DateTime(2020, 3, 30, 17, 2, 51, 929, DateTimeKind.Local).AddTicks(5872) });
+        }
+    }
+}
